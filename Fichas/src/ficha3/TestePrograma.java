63ad938a20d3d3722786d928c8a70cbdc5a5370c
Diogo-da-Silva-Rebelo <diogorebelo17@hotmail.com>
2021-04-13 21:15:30 +0100

    Ex2 Done.

--- Fichas/src/ficha3/TestePrograma.java
+++ Fichas/src/ficha3/TestePrograma.java
@@ -1,5 +1,6 @@
 package ficha3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestePrograma {
@@ -13,7 +14,6 @@ public class TestePrograma {
 
             // Como vamos ler sempre arrays, porque não criar um método que faça isso na classe Ficha2?
             switch (pergunta) {
-                /*
                 //A pergunta 0 é a pergunta relativa ao exemplo da implementação da Stack.
                 case 0:
                     Stack stk = new Stack();
@@ -33,7 +33,7 @@ public class TestePrograma {
 
                     System.out.println("Stk == stk2 ? "+ stk.equals(stk2));
                     System.out.println("Tamanho stack1 "+ stk.length());
-                    break; */
+                    break;
                 case 1:
                     Ex1 c = new Ex1();
                     c.setRaio(1);
@@ -46,7 +46,7 @@ public class TestePrograma {
                     System.out.println("Área: " + a);
 
                     Ex1 c2 = c.clone();
-                    System.out.println(" c == c  ? " + c.equals(c));
+                    //Ex: //System.out.println(" c == c  ? " + c.equals(c));
                     System.out.println(" c == c2 ? " + c.equals(c2));
                     c2.setRaio(5);
                     System.out.println(" c == c2 ? " + c.equals(c2));
@@ -54,7 +54,46 @@ public class TestePrograma {
                     System.out.println(" c == c2 ? " + c.equals(c2));
                     break;
                 case 2:
+                    Ex2 phone = new Ex2("Apple","3Ot5", 10, 30, 45, 1024, new ArrayList<>(), 2048, 4960, 2000, 18, 20, new ArrayList<>());
+                    System.out.println("New phone inserted: \n" + phone);
+
+                    int newAppBytes = 1000;
+                    System.out.println("Is there storage for an app with " + newAppBytes + "bytes? " + phone.isThereStorage(newAppBytes));
+
+                    phone.installApp("Instagram",30);
+                    phone.installApp("Pinterest",20);
+                    phone.installApp("Lightroom",10);
+                    phone.removeApp("Pinterest",20);
+
+                    double avgSize = phone.avgAppSize();
+                    int totalAppStorage = phone.getPhotoAndAppStorage();
+                    int usedStorage = phone.getUsedStorage();
+                    int freeSpace = totalAppStorage - usedStorage;
+                    System.out.println("You still have " + freeSpace + " free bytes.");
+                    System.out.println("Average App Size is " + avgSize);
+
+                    phone.receiveMessage("Hello there mate! How's everything going ? ");
+                    phone.receiveMessage("Hope you're okay ");
+                    phone.receiveMessage("I love you. ");
+                    phone.receiveMessage("XOXO, mum ");
+
+                    System.out.println("User decided to check messages!" + phone.getMessages());
+                    System.out.println("Biggest message is: " + phone.biggestMessage());
                     break;
+                case 9:
+                    Ponto a2 = new Ponto(0,0);
+                    Ponto b2 = new Ponto(0,2);
+                    Ponto c3 = new Ponto(5,0);
+                    Triangulo triangulo1 = new Triangulo(a2,b2,c3);
+                    a2.setX(-5);
+                    System.out.println("Triângulo definido pelos pontos (0,0), (0,2) e (5,0).");
+                    System.out.println(triangulo1 + "\n");
+                    System.out.println("Área do triângulo = " + triangulo1.calculaAreaTriangulo());
+                    System.out.println("Perímetro do triângulo = " + triangulo1.calculaPerimetroTriangulo());
+                    System.out.println("Altura do triângulo = " + triangulo1.calculaAltura());
+
+                    Triangulo triangulo2 = triangulo1.clone();
+                    if (!triangulo1.equals(triangulo2)) System.out.println("Something is wrong with the equals or the clone functions.");
 
                 default:
                     System.out.println("Input inválido.");
